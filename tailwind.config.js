22634389c953d2356d675fe1ea713728dfce8ba6
Anwar Hosen <anwarhossene@gmail.com>
2025-02-17 18:13:43 +0600

    removed tailwind cdn, added tailwind.css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    './js/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// npx tailwindcss -i ./css/input.css -o ./css/tailwind.css
+// docker run -d -p 1100:80 --name ProjectDKCB -v ./:/usr/share/nginx/html:ro --restart unless-stopped nginx:alpine
